1facb73bb9291969e8a4bc9b03317575492f387e
Carlos Augusto da Silva Junior <carlos.silvajunior@undf.edu.br>
2026-01-21 09:58:26 +0000

    Criação da tabela dim_adw__product na marts.

---
+++ models/marts/dim_adw__product.sql
@@ -0,0 +1,17 @@
+with
+    -- import CTES
+    int_products as (
+        select *
+        from {{ ref('int_adw__product_join') }}
+    ),
+    
+    dim_adw_products__metrics as (
+        select
+            produto_sk,
+            nome_produto,
+            nome_categoria,
+            nome_subcategoria,
+            preco_lista
+        from int_products
+    )
+select * from dim_adw_products__metrics
