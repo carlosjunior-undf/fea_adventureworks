a9a890207989728004f9e695f9e2777499b8927d
Carlos Augusto da Silva Junior <carlos.silvajunior@undf.edu.br>
2026-01-22 22:22:12 +0000

    Ateração na tabela stg_sales_salesorderdetail.sql na adw_sales/staging e alteração na tabela int_adw__salesorder_join na intermediate.

--- models/staging/adw_sales/stg_adw__sales_salesorderdetail.sql
+++ models/staging/adw_sales/stg_adw__sales_salesorderdetail.sql
@@ -2,6 +2,7 @@
     materialized="view",
     schema="stg_adw"
 ) }}
+
 with 
 
 source_sales_salesorderdetail as (
@@ -20,9 +21,9 @@ renamed as (
         cast(modifieddate as date) as data_completa,
         cast(orderqty as int) as quantidade_comprada,
         cast(unitprice as float) as preco_unitario,
-        cast(unitpricediscount as float) as desconto,
+        cast(unitpricediscount as float) as desconto_pct,
         (preco_unitario * quantidade_comprada) as valor_total_negociado,
-        (desconto * preco_unitario * quantidade_comprada) as desconto_pct
+        (preco_unitario * (1 - desconto_pct) * quantidade_comprada) as desconto_total
     from source_sales_salesorderdetail
 
 )
