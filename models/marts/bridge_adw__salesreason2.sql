08268fec4addd812e8682ed5994f8f3ca086057e
Carlos Augusto da Silva Junior <carlos.silvajunior@undf.edu.br>
2026-02-21 13:46:45 +0000

    - Alteração no modelo int_adw__sales2 na camada intermediate e nos modelos bridge_adw__salesreason2, dim_adw__salesreason2 e fct_adw__sales na camada marts. - Criação no modelo dim_adw__order2 na camada marts.

--- models/marts/bridge_adw__salesreason2.sql
+++ models/marts/bridge_adw__salesreason2.sql
@@ -1,7 +1,6 @@
 -- bridge_adw__salesreason.sql
 -- Tabela bridge para relação N:N entre fct_adw__sales e dim_adw__salesreason.
 -- Permite análise de múltiplos motivos por pedido no Power BI sem duplicar métricas.
-
 {{ config(
     materialized = "table",
     schema       = "marts_adw"
@@ -20,16 +19,27 @@ dim_salesreason as (
 
 ),
 
+dim_order as (
+
+    select salesorderid, order_sk
+    from {{ ref('dim_adw__order2') }}
+
+),
+
 -- granularidade: uma linha por salesorderid + salesreasonid
 final as (
 
     select
         {{ dbt_utils.generate_surrogate_key(['order_reason.salesorderid', 'order_reason.salesreasonid']) }} as bridge_sk
+        ,dim_order.order_sk
         ,order_reason.salesorderid
         ,dim_salesreason.salesreason_sk
 
     from order_reason
-    inner join dim_salesreason on order_reason.salesreasonid = dim_salesreason.salesreasonid
+    inner join dim_salesreason
+        on order_reason.salesreasonid = dim_salesreason.salesreasonid
+    inner join dim_order
+        on order_reason.salesorderid = dim_order.salesorderid
 
 )
 
